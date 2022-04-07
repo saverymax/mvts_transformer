2baab5166b3d35f792d3ee8876f51a2320fc8324
saverymax <saverymax@gmail.com>
2022-04-07 12:00:05 +0200

    Set up wandb logging

--- src/main.py
+++ src/main.py
@@ -39,22 +39,26 @@ import wandb
 
 def main(config):
 
-    #wandb_config = dict(
-    #    data_class=config['data_class'],
-    #    learning_rate=config['lr'],
-    #    d_model=config['d_model'],
-    #    max_len=config['max_seq_len'],
-    #    task=config['task'],
-    #)
+    if config['verbose']:
+        logging.info("Using verbose logging")
 
-    ## mix is my wandb username
-    #wandb.init(
-    #    project="mvts-forecasting",
-    #    entity="mix",
-    #    notes=config['comment'],
-    #    tags=["forecasting", 'transformer'],
-    #    config=wandb_config,
-    #) 
+    wandb_config = dict(
+        data_class=config['data_class'],
+        learning_rate=config['lr'],
+        d_model=config['d_model'],
+        max_len=config['max_seq_len'],
+        task=config['task'],
+        pollutant=config['pollutant'],
+    )
+
+    # mix is my wandb username
+    wandb.init(
+        project="mvts-forecasting",
+        run=config['comment'],
+        entity="mix",
+        tags=["forecasting", 'transformer'],
+        config=wandb_config,
+    ) 
 
     total_epoch_time = 0
     total_eval_time = 0
